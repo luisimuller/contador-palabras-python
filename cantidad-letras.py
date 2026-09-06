035f1af725491fc77f6e336e209b83e62a143046
luisimuller <luisina.e.muller@gmail.com>
2026-09-06 09:29:05 -0300

    Se guardó archivo cantidad-letras.py

--- cantidad-letras.py
+++ cantidad-letras.py
@@ -1,3 +1,9 @@
+
+#Paso 1: Solicitar entrada al usuario
 palabra_ingresada=input("Por favor ingrese una palabra: ")
+
+#Paso 2: Contar la cantidad de letras
 cantidad_letras=len(palabra_ingresada)
+
+#Paso 3: Mostrar al usuario el resultado
 print(f"La palabra {palabra_ingresada} tiene {cantidad_letras} letras.")
